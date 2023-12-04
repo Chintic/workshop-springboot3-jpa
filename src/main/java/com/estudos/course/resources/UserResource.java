7cf10f85325ffbe810324086474f02475b2f7b2c
Chintic <leonel.balduino@hotmail.com>
2023-12-04 16:39:43 -0300

    User entity and resource

---
+++ src/main/java/com/estudos/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.estudos.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudos.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	//GET (Vem do Rest: GET, POST, PUT e DELETE. Este é o GET
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
