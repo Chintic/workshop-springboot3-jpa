6ceaa8556da4649df4dedb9a71ad960615ab413e
Chintic <leonel.balduino@hotmail.com>
2023-12-05 16:23:00 -0300

    H2 database, teste profile, JPA

--- src/main/java/com/estudos/course/resources/UserResource.java
+++ src/main/java/com/estudos/course/resources/UserResource.java
@@ -11,10 +11,19 @@ import com.estudos.course.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	//GET (Vem do Rest: GET, POST, PUT e DELETE. Este é o GET
+	//GET (Vem do Rest: GET, POST, PUT e DELETE. Este é o GET	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
+	
+	/*@GetMapping
+	public User findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+		return u;
+	}
+	Esta maneira também funciona, mas assim o método não está retornando um ResponseEntity.
+	*/
+	
 }
