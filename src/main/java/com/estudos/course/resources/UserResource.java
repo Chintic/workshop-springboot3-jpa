656b349e4eed9ea877769d8ffa595cab1f472855
Chintic <leonel.balduino@hotmail.com>
2023-12-18 16:11:56 -0300

    Adjusting TestConfig

--- src/main/java/com/estudos/course/resources/UserResource.java
+++ src/main/java/com/estudos/course/resources/UserResource.java
@@ -1,21 +1,28 @@
 package com.estudos.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estudos.course.entities.User;
+import com.estudos.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	//GET (Vem do Rest: GET, POST, PUT e DELETE. Este é o GET	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
 	/*@GetMapping
