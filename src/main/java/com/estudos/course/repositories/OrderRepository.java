1f0dfdf66f0b8a3a8600457e7a2c6b807ee8dc21
Chintic <leonel.balduino@hotmail.com>
2023-12-21 11:59:50 -0300

    Order entity part 2

---
+++ src/main/java/com/estudos/course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.estudos.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudos.course.entities.Order;
+
+/* Aqui poderia colocar a Annotation @Repository, mas isso não é necessário pois a interface 
+já extende de JPARepository, que já tem uma Annotation equivalente */
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
